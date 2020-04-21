2a6bfb5e458e3bc7d170fd9d93761087c643efa2
Torsten Kilias <torsten.kilias@exasol.com>
2020-04-21 19:52:02 +0200

    Return complete stacktrace in tests/test/python3-ds-flavor/available-packages.py

--- tests/test/python3-ds-flavor/available-packages.py
+++ tests/test/python3-ds-flavor/available-packages.py
@@ -13,15 +13,20 @@ class AvailablePythonPackages(udf.TestCase):
 
     def import_test(self, pkg, fail=False, alternative=None):
         self.query(udf.fixindent('''
-            CREATE OR REPLACE PYTHON3 SCALAR SCRIPT available_packages.test_import_of_package() returns int AS
-            import %s
-            def run(ctx): return 1
+            CREATE OR REPLACE PYTHON3 SCALAR SCRIPT available_packages.test_import_of_package() returns VARCHAR(2000000) AS
+            def run(ctx):
+                try:
+                    import %s
+                    return "ok"
+                except Exception as e:
+                    import traceback
+                    return traceback.format_exc()
             /
             ''' % (pkg)))
         try:
             rows = self.query('''SELECT available_packages.test_import_of_package() FROM dual''')
             if not fail:
-                self.assertRowsEqual([(1,)], rows)
+                self.assertRowsEqual([("ok",)], rows)
             else:
                 assert 'Expected Failure' == 'not found'
         except:
